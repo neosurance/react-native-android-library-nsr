f82f18a2d14f74734e06b1e8f03f202474b7194c
ok_neosurance <ok_neosurance@ok-neosurance.local>
2019-05-27 10:32:42 +0200

    pacckage v1

--- android/src/main/java/eu/neosurance/sdk/Module.java
+++ android/src/main/java/eu/neosurance/sdk/Module.java
@@ -1,10 +1,14 @@
 package eu.neosurance.sdk;
 
+import android.util.Log;
 import android.widget.Toast;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +17,7 @@ public class Module extends ReactContextBaseJavaModule {
 
   private static final String DURATION_SHORT_KEY = "SHORT";
   private static final String DURATION_LONG_KEY = "LONG";
+  public static ReactApplicationContext ctx;
 
   public Module(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -20,7 +25,7 @@ public class Module extends ReactContextBaseJavaModule {
 
   @Override
   public String getName() {
-    return "Neosurance SDK";
+    return "Neosurance";
   }
 
   @Override
@@ -32,7 +37,64 @@ public class Module extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void show(String message, int duration) {
-    Toast.makeText(getReactApplicationContext(), message, duration).show();
+  public void show(final String message, final int duration) {
+
+    getReactApplicationContext().runOnUiQueueThread(new Runnable() {
+      @Override
+      public void run() {
+        //Toast.makeText(getReactApplicationContext(), message, duration).show();
+
+        ctx = getReactApplicationContext();
+        NSRSettings settings = new NSRSettings();
+        settings.setDisableLog(false);
+        settings.setDevMode(true);
+        settings.setBaseUrl("https://sbsdk.neosurancecloud.net/api/v1.0/");
+        settings.setCode("bppb");
+        settings.setSecretKey("pass");
+        settings.setPushIcon(R.drawable.nsr_logo);
+        //settings.setWorkflowDelegate(new WFDelegate(),ctx);
+        NSR.getInstance(ctx).setup(settings, new JSONObject());
+        NSR.getInstance(ctx).askPermissions(((ReactApplicationContext) ctx).getCurrentActivity());
+
+        NSRUser user = new NSRUser();
+        user.setEmail("mario@rossi.com");
+        user.setCode("mario@rossi.com");
+        user.setFirstname("Mario");
+        user.setLastname("Rossi");
+        user.setFiscalCode("RSSMRA85T01F205P");
+        JSONObject locals = new JSONObject();
+        try {
+          locals.put("email","mario@rossi.com");
+          locals.put("firstname","Mario");
+          locals.put("lastname","Rossi");
+          locals.put("fiscalCode","RSSMRA85T01F205P");
+          locals.put("pushToken","fake-push");
+          user.setLocals(locals);
+        } catch (JSONException e) {
+          e.printStackTrace();
+        }
+
+        NSR.getInstance(ctx).registerUser(user);
+
+        try {
+          Thread.sleep(10000);
+          Log.d("MODULE", "sendEvent");
+          JSONObject payload = new JSONObject();
+          payload.put("iata", "LIN");
+          NSR.getInstance(ctx).sendEvent("inAirport", payload);
+        } catch (Exception e) {
+          Log.e("MODULE", "sendEvent exception: " + e.getMessage());
+        }
+
+      }
+    });
+
+
   }
+
+  @ReactMethod
+  public void showLog(String message) {
+    Log.d("MODULE NSR",">>>>> " + message);
+  }
+
 }
